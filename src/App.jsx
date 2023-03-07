d0e72d120b1ed7192027637a16d968758ef638da
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-06 21:17:01 -0600

    Update react router to latest version

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Missions from './components/Missions';
 import Rockets from './components/Rockets';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import ErrorPage from './components/ErrorPage';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navbar />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Missions />,
+      },
+      {
+        path: 'missions',
+        element: <Missions />,
+      },
+      {
+        path: 'rockets',
+        element: <Rockets />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
-    <BrowserRouter className="app-container">
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Missions />} />
-        <Route path="rockets" element={<Rockets />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
