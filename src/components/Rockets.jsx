e3124a6099a378532d79ceb9de15cf5063a4a314
jorgeabrahan <jorge24abrahan@gmail.com>
2023-03-07 15:55:56 -0600

    Fetch rockets once when the page loads

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRockets } from '../redux/rockets/rocketsSlice';
+
 function Rockets() {
+  const dispatch = useDispatch();
+  const { rockets, status } = useSelector((store) => store.rockets);
+  useEffect(() => {
+    /* Validation to fetch data just when the page loads */
+    if (rockets.length > 0) return;
+    if (status === 'failed') return;
+    dispatch(fetchRockets());
+  }, [dispatch]);
+
   return (
     <section className="center">
       <div>
