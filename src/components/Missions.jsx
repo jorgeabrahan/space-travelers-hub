ec9520452608fdf4ec2c07bd165b3a9cb1d03677
NelsonAraujo93 <nelsonaraujoparedes93@hotmail.com>
2023-03-07 19:44:14 -0500

    Add extraReducer on missionsSlice to fetchMissionsAPI and dispatch it from missions.jsx

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { missionsFetchAPI } from '../redux/missions/missionsSlice';
+
 function Missions() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(missionsFetchAPI());
+  }, []);
+
   return (
     <section className="center"> </section>
   );
